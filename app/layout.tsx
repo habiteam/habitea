f4976df7ec2a2a16f558a3d14c54923c811a80c8
Nikodem Zawirski <nikon96@gmail.com>
2022-10-29 16:42:42 +0200

    FontAwesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import './globals.scss';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFaceAngry } from '@fortawesome/free-solid-svg-icons';
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <h1>
+      Hello world&nbsp;
+      <FontAwesomeIcon icon={faFaceAngry} />
+    </h1>
+  );
 }
