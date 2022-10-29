f2ccebf7d471bbc0e76d52dd763a7da75d1f1f78
Shirokone <foresai221@gmail.com>
2022-10-29 14:23:10 +0200

    fix: firestore

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 
+import { doc, getDoc } from 'firebase/firestore';
+import { database } from '../../common/services/firebase';
+
 export default function Home() {
-  function callDb() {
-    console.log('moshi moshi firebase');
+  async function callDb() {
+    const docRef = doc(database, 'Dogs', 'Husky');
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data());
   }
   return (
     <>
--- common/services/firebase.ts
+++ common/services/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import * as dotenv from 'dotenv';
+import { getFirestore } from 'firebase/firestore';
 
-dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +18,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export default initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
