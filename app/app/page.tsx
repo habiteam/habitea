47cca52f245050f527d6b1d8571895879554567c
Nikodem Zawirski <nikon96@gmail.com>
2022-10-29 16:30:11 +0200

    Testing db

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,18 +1,9 @@
 'use client';
 
-import { doc, getDoc } from 'firebase/firestore';
-import { database } from '../../common/services/firebase';
-
 export default function Home() {
-  async function callDb() {
-    const docRef = doc(database, 'Dogs', 'Husky');
-    const docSnap = await getDoc(docRef);
-    console.log(docSnap.data());
-  }
   return (
     <>
       <h1>Hello app</h1>
-      <button onClick={callDb}>Let&apos;s go</button>
     </>
   );
 }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { doc, getDoc } from 'firebase/firestore';
+import { database } from '../../common/services/firebase';
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse,
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  const docRef = doc(database, 'Dogs', 'Husky');
+  const docSnap = await getDoc(docRef);
+  res.status(200).json(docSnap.data());
 }
