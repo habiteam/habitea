254e6e870875fc5de9d1ade8ded07cd9826dce1c
Nikodem Zawirski <nikon96@gmail.com>
2022-11-05 16:21:59 +0100

    styles cleanup and refactor

--- common/atoms/screen-width.ts
+++ common/atoms/screen-width.ts
@@ -1,5 +1,9 @@
 import { atom } from 'jotai';
 
+/**
+ * TODO DRY: breakpoints also declared in scss
+ * @see styles/breakpoints.module.scss
+ */
 export const MOBILE_BREAKPOINT = 640;
 
 export const screenWidth = atom(0);
