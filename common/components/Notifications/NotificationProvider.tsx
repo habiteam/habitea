0c64e3dae11657357855d86f3d4c3c3d41674f9c
Shirokone <foresai221@gmail.com>
2022-11-05 23:00:49 +0100

    feat: notif anim

--- common/components/Notifications/NotificationProvider.tsx
+++ common/components/Notifications/NotificationProvider.tsx
@@ -1,4 +1,5 @@
 import { useAtomValue } from 'jotai';
+import { useTransition, animated, config } from 'react-spring';
 import styles from './NotificationProvider.module.scss';
 import notifications from '../../atoms/notifications';
 import { Notification } from './Notification';
@@ -10,11 +11,20 @@ export interface NotificationProviderPropSchema {
 export function NotificationProvider(props: NotificationProviderPropSchema) {
   const notificationList = useAtomValue(notifications);
 
+  const transitions = useTransition(notificationList, {
+    from: { opacity: 0, transform: 'translateY(100px) scaleY(0.2)' },
+    enter: { opacity: 1, transform: 'translateY(0) scaleY(1)' },
+    leave: { opacity: 0 },
+    config: config.gentle,
+  });
+
   return (
     <>
       <div className={styles.list}>
-        {notificationList.map((notification) => (
-          <Notification key={notification.id} {...notification} />
+        {transitions((style, notification) => (
+          <animated.div style={style} key={notification.id}>
+            <Notification {...notification} />
+          </animated.div>
         ))}
       </div>
       {props.children}
