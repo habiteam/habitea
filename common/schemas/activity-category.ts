728c14a7767c08f464ba0f32bcd3821b9aed0cf3
Shirokone <foresai221@gmail.com>
2022-12-18 19:56:09 +0100

    changed intefaces to class

--- common/schemas/activity-category.ts
+++ common/schemas/activity-category.ts
@@ -7,22 +7,70 @@ import {
 import { IconName } from '@fortawesome/fontawesome-svg-core';
 import { Timestamp } from 'firebase/firestore';
 
-export interface ActivityCategory {
+export class ActivityCategory {
   id: string;
+
   name: string;
+
   icon: IconName;
+
   description?: string;
+
   status: ActivityCategoryStatus;
+
   goalValue: string | number;
+
   goalType: ActivityCategoryGoalType;
+
   repeatType: ActivityCategoryRepeatType;
+
   unit: string;
+
   unitType: ActivityUnitType;
+
   duration: string;
+
   createdDate: Timestamp;
+
   createdBy: string;
+
   validFrom?: string;
+
   validTo?: string;
+
+  constructor(
+    id: string,
+    name: string,
+    icon: IconName,
+    description: string,
+    status: ActivityCategoryStatus,
+    goalValue: string | number,
+    goalType: ActivityCategoryGoalType,
+    repeatType: ActivityCategoryRepeatType,
+    unit: string,
+    unitType: ActivityUnitType,
+    duration: string,
+    createdDate: Timestamp,
+    createdBy: string,
+    validFrom: string,
+    validTo: string,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.icon = icon;
+    this.description = description;
+    this.status = status;
+    this.goalValue = goalValue;
+    this.goalType = goalType;
+    this.repeatType = repeatType;
+    this.unit = unit;
+    this.unitType = unitType;
+    this.duration = duration;
+    this.createdDate = createdDate;
+    this.createdBy = createdBy;
+    this.validFrom = validFrom;
+    this.validTo = validTo;
+  }
 }
 
 export type ActivityCategoryCreateFormType = Omit<
--- common/schemas/activity.ts
+++ common/schemas/activity.ts
@@ -1,10 +1,31 @@
 import { Timestamp } from 'firebase/firestore';
 
-export interface Activity {
+export class Activity {
   id: string;
+
   categoryRef: string;
+
   value: string;
+
   activityDate: Timestamp;
+
   createdDate: Timestamp;
+
   createdBy: string;
+
+  constructor(
+    id: string,
+    categoryRef: string,
+    value: string,
+    activityDate: Timestamp,
+    createdDate: Timestamp,
+    createdBy: string,
+  ) {
+    this.id = id;
+    this.categoryRef = categoryRef;
+    this.value = value;
+    this.activityDate = activityDate;
+    this.createdDate = createdDate;
+    this.createdBy = createdBy;
+  }
 }
