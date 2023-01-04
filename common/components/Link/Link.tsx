1eb31c8deb5fe414eca02a4b47f1704e3e32a09a
Nikodem Zawirski <nikon96@gmail.com>
2023-01-04 18:56:11 +0100

    MP layout rework
    Some random seo texts for main page

--- common/components/Link/Link.tsx
+++ common/components/Link/Link.tsx
@@ -7,6 +7,8 @@ import styles from './Link.module.scss';
 export interface LinkPropSchema {
   children: React.ReactNode;
   href: string | UrlObject;
+  target?: '_blank' | '_self' | '_parent' | '_top';
+  rel?: string;
   disabled?: boolean;
   color?: Color;
 }
@@ -15,6 +17,8 @@ export default function Link(props: LinkPropSchema) {
   return (
     <NextLink href={props.href} legacyBehavior>
       <a
+        target={props.target}
+        rel={props.rel}
         className={`${styles.link} ${
           styles[`link--${props.color ?? 'default'}`]
         }`}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image';
 import classNames from 'classnames';
 import Router from 'next/router';
 import heroBgImg from '@public/backgrounds/bg-main.jpg';
+import blobImg from '@public/backgrounds/bg-blobs.svg';
 import { auth } from '@services/firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import styles from './Home.module.scss';
 
 export default function Home() {
@@ -15,32 +19,85 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.hero}>
+      <div className={classNames(styles.container, styles['gradient-one'])}>
         <Image
           className={styles['background-image']}
           src={heroBgImg}
           alt={'background image'}
           placeholder="blur"
           fill
+          style={{ objectPosition: 'left' }}
         ></Image>
-        <div className={styles.hello}>
-          <h1 className={classNames(styles.title)}>Habitea</h1>
-          <h2 className={classNames(styles.subtitle)}>
-            Get in control of your habits
-          </h2>
-          <span className={styles.quote}>
-            “Until you make the unconscious conscious, it will direct
-            your&nbsp;life and you will call it fate.”
+        <div className={classNames(styles.grid)}>
+          <span className={classNames(styles.quote)}>
+            The future doesn&apos;t just happen. We are building it, and we are
+            building it all the time. -Hannah Fry
           </span>
-          <button
-            onClick={() => {
-              Router.push(auth.currentUser ? '/app' : '/register');
-            }}
-            className={classNames(styles.cta)}
-            color="default"
-          >
-            <strong>Get started</strong>
-          </button>
+        </div>
+        <div className={classNames(styles.grid)}>
+          <div className={classNames(styles.section)}>
+            <h1 className={classNames(styles.title)}>Habitea</h1>
+            <h2 className={classNames(styles.subtitle)}>
+              Activity and habit tracker
+            </h2>
+            <p>
+              <strong>Tracking your habits</strong> can help you identify
+              patterns, make changes, stay motivated, and build new habits. By
+              tracking your habits, you can stay accountable and motivated as
+              you work towards your goals
+            </p>
+            <p>
+              <strong>Habitea</strong> is a{' '}
+              <strong>simple and easy-to-use</strong> habit tracker that will
+              help you take control of your life and make positive changes that
+              last.
+            </p>
+            <button
+              onClick={() => {
+                Router.push(auth.currentUser ? '/app' : '/register');
+              }}
+              className={classNames(styles.cta)}
+              color="default"
+            >
+              <strong>Get started</strong>
+            </button>
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div className={classNames(styles.container, styles['gradient-two'])}>
+        <Image
+          className={styles['background-image']}
+          src={blobImg}
+          alt={'background image'}
+          fill
+        ></Image>
+        <div className={classNames(styles.grid)}>
+          <span className={classNames(styles.quote)}>
+            Be the designer of your world and not merely the consumer of it.
+            -James Clear
+          </span>
+        </div>
+        <div className={classNames(styles.grid)}>
+          <div className={classNames(styles.section)}>
+            <h1 className={classNames(styles.title)}>Habitea</h1>
+            <h2 className={classNames(styles.subtitle)}>Made for you</h2>
+            <p>
+              Habitea is free and open source software.
+              <br />
+              Have suggestions, ideas or want to contribute?
+            </p>
+            <a
+              href="https://github.com/habiteam/habitea"
+              target="_blank"
+              rel="noreferrer"
+              className={classNames(styles['gh-link'])}
+            >
+              <FontAwesomeIcon icon={faGithub as IconProp}></FontAwesomeIcon>
+              &nbsp; Visit our GitHub repository
+            </a>
+          </div>
+          <div className={classNames(styles.section)}></div>
         </div>
       </div>
     </>
