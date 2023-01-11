e3fb8683c18b5d5724e7f2a49ac822740329d5f6
Nikodem Zawirski <nikon96@gmail.com>
2023-01-11 17:03:10 +0100

    heatmap: color days based on habit strength

--- common/components/Heatmap/Heatmap.tsx
+++ common/components/Heatmap/Heatmap.tsx
@@ -45,26 +45,32 @@ export default function Heatmap(props: HeatmapProps) {
     }
     activitiesByDay[day].push(activity);
   });
+  // reduce activities in each day to it's strength
+  const activityStrengthsByDay = activitiesByDay.map((a) => {
+    if (!a) {
+      return 0;
+    }
+    return a.reduce((acc, activity) => acc + activity.value, 0);
+  });
+
   const days = [];
   const lastDayIndex = getLastDayOfMonth(currentDate).getDate();
   for (let i = 1; i <= lastDayIndex; i += 1) {
-    // TODO add tooltips showing dates
+    // TODO add tooltips showing dates and maybe activities
     days.push(
-      <div
-        key={i}
-        className={classNames(styles.days__day, {
-          [styles['days__day--compact']]:
-            activitiesByDay[i] && activitiesByDay[i].length > 4,
-        })}
-      >
-        {activitiesByDay[i] ? (
+      <div key={i} className={styles['item-wrapper']}>
+        {activityStrengthsByDay[i] ? (
           <div
             key={i}
             className={classNames(styles.item, {
               [styles['item--good']]: props.category?.goalType === 'MIN',
               [styles['item--bad']]: props.category?.goalType === 'MAX',
             })}
-          ></div> // TODO calculate "strength" of activities that day
+            style={{
+              opacity: activityStrengthsByDay[i] / props.category.goalValue,
+              // TODO more vibrant colors
+            }}
+          ></div>
         ) : (
           <div className={classNames(styles.item, styles['item--empty'])}></div>
         )}
