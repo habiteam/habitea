e49413e7ee769e4a2d6cdc87760da10a73e8f26a
Nikodem Zawirski <nikon96@gmail.com>
2023-01-15 21:03:30 +0100

    date utils

--- common/utils/date.ts
+++ common/utils/date.ts
@@ -6,6 +6,14 @@ export function getNextMonth(date: Date): Date {
   return new Date(date.getFullYear(), date.getMonth() + 1, 1);
 }
 
+export function getPreviousYear(date: Date): Date {
+  return new Date(date.getFullYear() - 1, date.getMonth(), 1);
+}
+
+export function getNextYear(date: Date): Date {
+  return new Date(date.getFullYear() + 1, date.getMonth(), 1);
+}
+
 export function getFirstDayOfMonth(date: Date): Date {
   return new Date(date.getFullYear(), date.getMonth(), 1);
 }
@@ -28,4 +36,26 @@ export function getFirstDayOfWeek(date: Date): Date {
     date.getMonth(),
     date.getDate() - date.getDay(),
   );
+}
+
+export function getLastDayOfYear(date: Date): Date {
+  return new Date(date.getFullYear(), 11, 31);
+}
+
+export function getFirstDayOfYear(date: Date): Date {
+  return new Date(date.getFullYear(), 0, 1);
+}
+
+export function getDayOfYear(date: Date): number {
+  return Math.floor(
+    (date.getTime() - new Date(date.getFullYear(), 0, 0).getTime()) /
+      1000 /
+      60 /
+      60 /
+      24,
+  );
+}
+
+export function getDateFromDayOfYear(year: number, day: number): Date {
+  return new Date(year, 0, day);
 }
